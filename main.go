8e656845f61d6c2101eeb1cee0943b0b1bf6fc07
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-09-14 23:22:49 +0200

    corm.go: update doc

--- main.go
+++ main.go
@@ -6,19 +6,32 @@ import (
 )
 
 type Config struct {
-	Host       string
+	// default "couch"
+	Host string
+	// default "http://localhost:5984/"
 	DriverName string
 	DBName     string
 }
 
 type ClientConfig struct {
-	Host       string
+	// default "couch"
+	Host string
+	// default "http://localhost:5984/"
 	DriverName string
 }
 
 var defaultDriverName = "couch"
 var defaultHostName = "http://localhost:5984/"
 
+// New returns a db instance connection by passed DBName.
+// When it fails it will return an error. Optionally "Host"
+// and DriverName can be passed with Config struct.
+//
+// 	ctx := context.TODO()
+//
+// 	db, err := New(ctx, Config{
+// 		DBName: "honeyglass",
+// 	})
 func New(ctx context.Context, config Config) (*Orm, error) {
 
 	initDefaults(&config)
