e59effc41e1c0fee05939a8fc2f242d9a2340f8e
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-11-19 23:50:07 +0100

    refactor; improvements

--- corm.go
+++ corm.go
@@ -28,43 +28,49 @@ type Orm struct {
 // it will return an id and revision. On fail it will return an error.
 // When doc contain the Id property, the database will interpret it as
 // predefined unique Id and when its omitted it will generate it automatically.
-func (c *Orm) Save(ctx context.Context, doc interface{}) (newId string, rev string, err error) {
+func (c *Orm) Save(ctx context.Context, doc interface{}) (docId string, docRev string, err error) {
 
-	structDoc := structs.New(doc)
-	id := structDoc.Field("Id").Value().(string)
-	Type := structDoc.Field("Type").Value().(string)
+	structName, docId, docRev, docType, err := CheckDocument(doc)
+	if err != nil {
+		return "", "", err
+	}
 
-	// 1.) Das Feld muss so in der Form existieren
-	// 1.) Darf kein Inhalt haben weil es intern genutzt wird
-	fmt.Fprintln(Type)
+	// docType must not be empty or it should be equal structName
+	if docType != "" && structName != docType {
+		return "", "", errDocAndTypeNotSameOrEmpty
+	}
 
 	// when docId already exists
-	_, err = c.Db.Get(ctx, id)
-	if id != "" && err == nil {
-		return "", "", fmt.Errorf(errDocIdAlreadyExists, id, structDoc.Name())
+	_, err = c.Db.Get(ctx, docId)
+	if docId != "" && err == nil {
+		return "", "", fmt.Errorf(errDocIdAlreadyExists, docId, structName)
 	}
 
 	// create doc with predefined id
-	if id != "" {
-		rev, err = c.Db.Put(ctx, id, doc)
-		return id, rev, err
+	if docId != "" {
+		docRev, err = c.Db.Put(ctx, docId, doc)
+		return docId, docRev, err
 	}
 
 	// create doc with auto-generated id
-	docId, rev, err := c.Db.CreateDoc(ctx, doc)
-	return docId, rev, err
+	docId, docRev, err = c.Db.CreateDoc(ctx, doc)
+	return docId, docRev, err
 }
 
 // Read reads document from database by passed id and doc struct.
 // When it fails it will return an empty Row and error and when
 // everything works fine, it will unmarshal the result (row) to
 // passed doc struct.
-func (c *Orm) Read(ctx context.Context, id string, doc interface{}, options ...Options) (row *Row, err error) {
+func (c *Orm) Read(ctx context.Context, docId string, doc interface{}, options ...Options) (row *Row, err error) {
 
-	if id == "" {
-		return &Row{}, errDocIdRequired
+	if docId == "" {
+		return nil, errDocIdRequired
 	}
-	row, err = c.Db.Get(ctx, id, options...)
+	row, err = c.Db.Get(ctx, docId, options...)
+	if err != nil {
+		return nil, errDocIdNotFound
+	}
+
 	// convert row to passed struct type
 	row.ScanDoc(&doc)
 	return row, err
@@ -73,31 +79,79 @@ func (c *Orm) Read(ctx context.Context, id string, doc interface{}, options ...O
 // Update updates doc (struct) as document in the database. The doc must
 // contains the doc Id and Rev property. When its saved correctly it will return a
 // new revision. Otherwise it will return an error.
-func (c *Orm) Update(ctx context.Context, doc interface{}) (newRev string, err error) {
+func (c *Orm) Update(ctx context.Context, doc interface{}) (docId, newRev string, err error) {
 
-	// extract id and rev from empty interface
-	structDoc := structs.New(doc)
-	id := structDoc.Field("Id").Value().(string)
-	rev := structDoc.Field("Rev").Value().(string)
-	Type := structDoc.Field("Type").Value().(string)
-
-	// 1.) Das Feld muss so in der Form existieren
-	// 1.) Darf kein Inhalt haben weil es intern genutzt wird
-	fmt.Fprintln(Type)
-
-	if id == "" && rev == "" {
-		return "", errDocIdAndRevRequired
+	structName, docId, docRev, docType, err := CheckDocument(doc)
+	if err != nil {
+		return "", "", err
 	}
-	return c.Db.Put(ctx, id, doc)
+
+	// document type must not be empty or it should be equal structName
+	if docType != "" && structName != docType {
+		return "", "", errDocAndTypeNotSameOrEmpty
+	}
+
+	// document id and document revision are required
+	if docId == "" && docRev == "" {
+		return "", "", errDocIdAndRevRequired
+	}
+
+	newRev, err = c.Db.Put(ctx, docId, doc)
+	return docId, newRev, err
 }
 
 // Delete deletes the document by passed id and revision (rev).
 // When id and revision (rev) are empty it will return an error.
 // When everything works fine it will return the new deleted revision (newRev).
-func (c *Orm) Delete(ctx context.Context, id, rev string) (newRev string, err error) {
+func (c *Orm) Delete(ctx context.Context, docId string, docRev ...string) (newRev string, err error) {
 
-	if id == "" && rev == "" {
-		return "", errDocIdAndRevRequired
+	type IdRev struct {
+		Id  string `json:"_id,omitempty"`
+		Rev string `json:"_rev,omitempty"`
 	}
-	return c.Db.Delete(ctx, id, rev)
+
+	_docRev := ""
+	docRevLen := len(docRev)
+
+	// keep passed docRev but when no docRev passed
+	// determine it by
+	if docRevLen >= 1 {
+		_docRev = docRev[0]
+	} else {
+		idRev := IdRev{}
+		_, err = c.Read(ctx, docId, &idRev)
+		if err != nil {
+			return "", err
+		}
+		_docRev = idRev.Rev
+	}
+
+	if docId == "" {
+		return "", errDocIdRequired
+	}
+
+	return c.Db.Delete(ctx, docId, _docRev)
+}
+
+// CheckDocument checks if the required fields Id, Rev and Type are exists.
+// When everything gone right, it return the extracted fields Id, Rev and Type.
+// When it fails it return an error.
+func CheckDocument(doc interface{}) (structName, docId, docRev, docType string, err error) {
+
+	structDoc := structs.New(doc)
+
+	// Check require field props exists
+	IdField, IdOk := structDoc.FieldOk("Id")
+	RevField, RevOk := structDoc.FieldOk("Rev")
+	TypeField, TypeOk := structDoc.FieldOk("Type")
+
+	if !IdOk || !RevOk || !TypeOk {
+		return "", "", "", "", fmt.Errorf(errDocIdRevTypeDynRequired, structDoc.Name())
+	}
+
+	docId = IdField.Value().(string)
+	docRev = RevField.Value().(string)
+	docType = TypeField.Value().(string)
+
+	return structDoc.Name(), docId, docRev, docType, err
 }
--- errors.go
+++ errors.go
@@ -3,7 +3,10 @@ package corm
 import "errors"
 
 var (
-	errDocIdRequired       = errors.New("doc.Id are required")
-	errDocIdAndRevRequired = errors.New("doc.Id and doc.rev are required")
-	errDocIdAlreadyExists  = "doc.Id: %v for `%v` struct document already created"
+	errDocIdRequired            = errors.New("doc.Id are required")
+	errDocIdNotFound            = errors.New("doc.Id not found")
+	errDocIdAndRevRequired      = errors.New("doc.Id and doc.rev are required")
+	errDocAndTypeNotSameOrEmpty = errors.New("doc.Type is reserved for corm")
+	errDocIdRevTypeDynRequired  = "doc.Id, doc.Rev and doc.Type: for `%v` struct document are required"
+	errDocIdAlreadyExists       = "doc.Id: %v for `%v` struct document already created"
 )
